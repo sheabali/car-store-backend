d6a958bbf6cf8000c1ddfb18cfffe8836b596f9d
sheabali <iamskpranto@gmail.com>
2025-02-04 22:52:08 +0600

    shurjopay.d.ts

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -11,4 +11,11 @@ export default {
   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
   jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  sp: {
+    sp_endpoint: process.env.SP_ENDPOINT,
+    sp_username: process.env.SP_USERNAME,
+    sp_password: process.env.SP_PASSWORD,
+    sp_prefix: process.env.SP_PREFIX,
+    sp_return_url: process.env.SP_RETURN_URL,
+  },
 };
---
+++ src/app/modules/order/order.utils.ts
@@ -0,0 +1,10 @@
+import Shurjopay from 'shurjopay';
+import config from '../../config';
+
+Shurjopay.config(
+  config.sp.sp_endpoint!,
+  config.sp.sp_username!,
+  config.sp.sp_password!,
+  config.sp.sp_prefix!,
+  config.sp.sp_return_url!,
+);
